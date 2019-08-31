6541d6accbad7929d4b0afb7dbb61d49c7e3066b
seannowotny <seannowotny@live.de>
2019-08-31 19:02:15 +0200

    Add incomplete factories for Author and Profile

---
+++ database/factories/AuthorFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Author;
+use Faker\Generator as Faker;
+
+$factory->define(Author::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
+
+$factory->afterCreating(Author::class, function($author)
+{
+    $author->profile()->save(factory(App\Profile::class)->make());
+});
+
+// $factory->afterMaking(Author::class, function($author)
+// {
+//     $author->profile()->save(factory(App\Profile::class)->make());
+// });
