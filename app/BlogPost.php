e861ddcbeb8dfc7b7865567f1b9f8466caad9393
seannowotny <seannowotny@live.de>
2019-09-02 14:11:59 +0200

    Make BlogPost and Comment models soft deleting

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -3,13 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['title', 'content'];
 
     public function comments()
     {
         return $this->hasMany('App\Comment');
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        // static::deleting(function(BlogPost $blogPost)
+        // {
+        //     $blogPost->comments()->delete();
+        // });
+
+        static::restoring(function(BlogPost $blogPost)
+        {
+            $blogPost->comments()->restore();
+        });
     }
 }
