aa9dd994e9ae718578f50949671fc60ee16d852a
seannowotny <seannowotny@live.de>
2019-09-11 19:10:40 +0200

    Make added at less confusing

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,6 +4,8 @@
     <h1>{{ $post->title }}</h1>
     <h5>{{ $post->content }}</h5>
 
+    <h5>Added {{ $post->created_at->diffForHumans() }}</h5>
+
     <h3>Comments</h3>
     @forelse ($post->comments as $comment)
         <p>{{ $comment->content }}</p>
@@ -11,8 +13,6 @@
     @empty
         <p>No Comments yet!</p>
     @endforelse
-
-    <h5>Added {{ $post->created_at->diffForHumans() }}</h5>
 
     @if(($diff = (new Carbon\Carbon())->diffInMinutes($post->created_at)) < 5)
         <h4>New!</h4>
