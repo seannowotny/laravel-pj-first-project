b59d741f72ef4a6640eba9201727d04ebc41a088
seannowotny <seannowotny@live.de>
2019-09-11 19:13:42 +0200

    Correct method name

--- app/User.php
+++ app/User.php
@@ -37,7 +37,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function blogPost()
+    public function blogPosts()
     {
         return $this->hasMany('App\BlogPost');
     }
