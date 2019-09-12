a010cc5bd34b67178a4d80b06df482bfea5fb5ac
seannowotny <seannowotny@live.de>
2019-09-12 18:47:29 +0200

    Add most commented card through local query scope to posts page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,41 +1,64 @@
 @extends('layout')
 
 @section('content')
-    @forelse($posts as $post)
-        <h3 class="mt-3">
-            <a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a>
-        </h3>
+    <div class="row">
+        <div class="col-8">
+            @forelse($posts as $post)
+                <h3 class="mt-3">
+                    <a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a>
+                </h3>
 
-        <p class="text-muted">
-            Added {{ $post->created_at->diffForHumans() }}
-            by {{ $post->user->name }}
-        </p>
+                <p class="text-muted">
+                    Added {{ $post->created_at->diffForHumans() }}
+                    by {{ $post->user->name }}
+                </p>
 
-        @if ($post->comments_count)
-            <p>{{ $post->comments_count }} comments</p>
-        @else
-            <p>No comments yet!</p>
-        @endif
-            
-        @can('update', $post)
-            <a href="{{ route('posts.edit', ['post' => $post->id]) }}" 
-                class="btn btn-primary">Edit</a>
-        @endcan
+                @if ($post->comments_count)
+                    <p>{{ $post->comments_count }} comments</p>
+                @else
+                    <p>No comments yet!</p>
+                @endif
+                    
+                @can('update', $post)
+                    <a href="{{ route('posts.edit', ['post' => $post->id]) }}" 
+                        class="btn btn-primary">Edit</a>
+                @endcan
 
-        {{-- @cannot('delete', $post)
-            <p>You cannot delete this post</p>
-        @endcannot --}}
+                {{-- @cannot('delete', $post)
+                    <p>You cannot delete this post</p>
+                @endcannot --}}
 
-        @can('delete', $post)
-            <form method="POST" class="fm-inline"
-                action="{{ route('posts.destroy', ['post' => $post->id]) }}">
-                @csrf
-                @method("DELETE")
+                @can('delete', $post)
+                    <form method="POST" class="fm-inline"
+                        action="{{ route('posts.destroy', ['post' => $post->id]) }}">
+                        @csrf
+                        @method("DELETE")
 
-                <input type="submit" class="btn btn-primary" value="Delete!"/>
-            </form>
-        @endcan
-    @empty
-        <p>No blog posts yet!</p>
-    @endforelse
+                        <input type="submit" class="btn btn-primary" value="Delete!"/>
+                    </form>
+                @endcan
+            @empty
+                <p>No blog posts yet!</p>
+            @endforelse
+        </div>
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Most Commented</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">
+                        What people are currently talking about
+                    </h6>
+                </div>
+                <ul class="list-group list-group-flush">
+                    @foreach($mostCommented as $post)
+                        <li class="list-group-item">
+                            <a href="{{ route('posts.show', ['post' => $post->id]) }}">
+                                {{ $post->title }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
 @endsection
