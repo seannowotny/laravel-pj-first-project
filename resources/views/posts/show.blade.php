d701cc346b08eb40cb115ddea799b7758ad80903
seannowotny <seannowotny@live.de>
2019-09-14 20:11:03 +0200

    Make blade files neater

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,21 +1,22 @@
 @extends('layout')
 
 @section('content')
-    <h1>{{ $post->title }}</h1>
-    <h5>{{ $post->content }}</h5>
+  <h1>{{ $post->title }}</h1>
+  <h5>{{ $post->content }}</h5>
 
-    <h5>Added {{ $post->created_at->diffForHumans() }}</h5>
+  <h5>Added {{ $post->created_at->diffForHumans() }}</h5>
 
-    <h3>Comments</h3>
-    @forelse ($post->comments as $comment)
-        <p>{{ $comment->content }}</p>
-        <p class="text-muted">Added {{ $comment->created_at->diffForHumans() }}</p>
-    @empty
-        <p>No Comments yet!</p>
-    @endforelse
+  @if(($diff = (new Carbon\Carbon())->diffInMinutes($post->created_at)) < 520)
+    @badge
+        New!
+    @endbadge
+  @endif
 
-    @if(($diff = (new Carbon\Carbon())->diffInMinutes($post->created_at)) < 5)
-        <h4>New!</h4>
-        <h5>Created {{ $diff }} minutes ago</h5>
-    @endif
+  <h3>Comments</h3>
+  @forelse ($post->comments as $comment)
+    <p>{{ $comment->content }}</p>
+    <p class="text-muted">Added {{ $comment->created_at->diffForHumans() }}</p>
+  @empty
+    <p>No Comments yet!</p>
+  @endforelse
 @endsection
