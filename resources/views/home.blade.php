f83d3a89eff09c9acef0b7e123f5c6d817300169
seannowotny <seannowotny@live.de>
2019-09-29 16:54:58 +0200

    First translation with JSON

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,9 @@
 <p>{{ trans_choice('messages.plural', 1, ['a' => 1]) }}</p>
 <p>{{ trans_choice('messages.plural', 2, ['a' => 1]) }}</p>
 
+<p>Using JSON: {{ __('Welcome to Laravel!') }}</p>
+<p>Using JSON: {{ __('Hello :name', ['name' => 'Sean']) }}</p>
+
 <p>This is the content of the main page!</p>
 @endsection
 
