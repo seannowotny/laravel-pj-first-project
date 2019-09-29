d5a6ef2e849d576475b45f3f10656d009f7c5f25
seannowotny <seannowotny@live.de>
2019-09-29 16:47:48 +0200

    First fake translations

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return[
+    'welcome' => 'Bienvenido a Laravel!',
+    'example_with_value' => 'Hola :name',
+    'plural' => '{0} No commenarios :a|{1} :count commentario :a|[2,*] :count commentarios :a',
+];
